c96a30589b1830c2b95531894fb26e6b6f1d1422
KenWin <kent3matsu@gmail.com>
2024-09-16 10:06:03 +0900

    Added PSM Component

---
+++ Source/RingRider/Private/Components/PsmComponent.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/PsmComponent.h"
+
+
+FPsmInfo::FPsmInfo(float DeltaTime, EPsmCondition Condition)
+	:DeltaTime(DeltaTime), Condition(Condition)
+{
+}
+
+
+// Sets default values for this component's properties
+UPsmComponent::UPsmComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+// Called when the game starts
+void UPsmComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+// Called every frame
+void UPsmComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (StateMap.Num() == 0)
+	{
+		return;
+	}
+
+	for (const auto& State : StateMap)
+	{
+		bool bIsOn = State.Value;
+		if (bIsOn)
+		{
+			FPsmInfo PsmInfo(DeltaTime, EPsmCondition::STAY);
+			(*State.Key)(PsmInfo);
+		}
+	}
+}
+
+
+void UPsmComponent::AddState(TStateFunc& StateFunc)
+{
+	StateMap.Add(&StateFunc, false);
+}
+
+void UPsmComponent::RemoveState(TStateFunc& StateFunc)
+{
+	StateMap.Remove(&StateFunc);
+}
+
+void UPsmComponent::ClearState()
+{
+	StateMap.Empty();
+}
+
+
+void UPsmComponent::TurnOnState(TStateFunc& StateFunc)
+{
+	if (StateMap.Contains(&StateFunc))
+	{
+		bool* bIsOn = &StateMap[&StateFunc];
+		if (!(*bIsOn))
+		{
+			FPsmInfo PsmInfo(0.f, EPsmCondition::ENTER);
+			StateFunc(PsmInfo);	// ENTER process.
+			*bIsOn = true;		// STAY process starts here.
+		}
+	}
+}
+
+void UPsmComponent::TurnOffState(TStateFunc& StateFunc)
+{
+	if (StateMap.Contains(&StateFunc))
+	{
+		bool* bIsOn = &StateMap[&StateFunc];
+		if (*bIsOn)
+		{
+			FPsmInfo PsmInfo(0.f, EPsmCondition::EXIT);
+			*bIsOn = false;		// STAY process ends here.
+			StateFunc(PsmInfo);	// EXIT process.
+		}
+	}
+}
+
+void UPsmComponent::TurnOnAll()
+{
+	for (auto& State : StateMap)
+	{
+		TurnOnState(*State.Key);
+	}
+}
+
+void UPsmComponent::TurnOffAll()
+{
+	for (auto& State : StateMap)
+	{
+		TurnOffState(*State.Key);
+	}
+}
