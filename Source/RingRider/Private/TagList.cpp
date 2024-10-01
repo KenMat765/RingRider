844654aef6f40c27e05d8616ea925f807bbba840
KenWin <kent3matsu@gmail.com>
2024-10-02 07:13:09 +0900

    Tagを管理する構造体TagListを追加

---
+++ Source/RingRider/Private/TagList.cpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "TagList.h"
+
+const FName FTagList::TAG_RIDER(TEXT("Rider"));
+const FName FTagList::TAG_HEXTILE(TEXT("HexTile"));
+const FName FTagList::TAG_BOUNCE(TEXT("Bounce"));
---
+++ Source/RingRider/Public/TagList.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+struct FTagList
+{
+	static const FName TAG_RIDER;
+	static const FName TAG_HEXTILE;
+	static const FName TAG_BOUNCE;
+};
